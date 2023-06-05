a4c998b56e887b2e60f69f67fd63da9eca77da07
Anton Nekipelov <226657+anton-107@users.noreply.github.com>
2023-06-05 16:36:27 +0200

    JSON API reader empty class

---
+++ src/json-api-reader.ts
@@ -0,0 +1,5 @@
+export class JSONAPIReader {
+  public version(): number {
+    return 1;
+  }
+}
--- test/jsonapi-reader.test.ts
+++ test/jsonapi-reader.test.ts
@@ -1,8 +1,11 @@
 import assert from "node:assert/strict";
 import { describe, it } from "node:test";
 
+import { JSONAPIReader } from "../src/json-api-reader";
+
 describe("JSON API reader", () => {
-  it("works", () => {
-    assert.equal(1, 1);
+  it("instntiates", () => {
+    const reader = new JSONAPIReader();
+    assert.equal(reader.version(), 1);
   });
 });
