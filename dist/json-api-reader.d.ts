74734231c13fa275c3343ec35f1dfc8e41a662a9
Anton Nekipelov <226657+anton-107@users.noreply.github.com>
2023-06-05 16:43:54 +0200

    enable typescript build

---
+++ dist/json-api-reader.d.ts
@@ -0,0 +1,3 @@
+export declare class JSONAPIReader {
+    version(): number;
+}
---
+++ dist/json-api-reader.js
@@ -0,0 +1,6 @@
+export class JSONAPIReader {
+    version() {
+        return 1;
+    }
+}
+//# sourceMappingURL=json-api-reader.js.map
